98ebe262266dd59d240d829da8333fc9e2ac88f0
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-03-16 00:18:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,10 @@ class Scraper
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
+  def get_courses
+    self.get_page.css(".post")
+  end
+  
   
   
   def print_courses
